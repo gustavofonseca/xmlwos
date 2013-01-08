9e090894504bec940f34425397c3711919e105c2
fabiobatalha <fabiobatalha@gmail.com>
2013-01-08 13:32:10 -0200

    ajustando aplicações em diretórios

--- sh/mongodb_start.sh
+++
@@ -1,1 +0,0 @@
-nohup mongod --port 28018 --dbpath ../databases/mongodb &> mongodb.out &
--- sh/xmlwos_config.sh
+++
@@ -1,2 +0,0 @@
-export cisis_dir="/usr/local/bireme/cisis/5.4.pre04/lindG4/"
-export scielo_data_url="localhost:7000"
