da9325449f6c523010f19afeee552ef8d3fff0e8
fabiobatalha <fabiobatalha@gmail.com>
2013-01-11 17:44:28 -0200

    New Schema version

--- export2wos/tools.py
+++ export2wos/tools.py
@@ -1,3 +1,4 @@
+import urllib2
 from pymongo import Connection
 from porteira.porteira import Schema
 
@@ -14,21 +15,32 @@ def get_collection(mongodb_host='localhost',
     return coll
 
 
-def find(fltr, collection):
-    for article in collection.find(fltr, {'code': 1}):
+def find(fltr, collection, skip, limit):
+    for article in collection.find(fltr, {'code': 1}).skip(skip).limit(limit):
         yield article['code']
 
 
-def validate_xml(xml):
+def validate_xml(article_id):
     """
     Validate article agains WOS Schema. Flaging his attribute validated_scielo to True if
     the document is valid.
     """
-    sch = Schema('xsd/ThomsonReuters_publishing.xsd')
-    return sch.validate(xml)
+    xsd = open('ThomsonReuters_publishing.xsd', 'r').read()
+    sch = Schema(xsd)
+
+    xml_url = 'http://200.136.72.76:7000/api/v1/article?code={0}&format=xml'.format(article_id)
+
+    xml = urllib2.urlopen(xml_url, timeout=2).read()
+
+    result = sch.validate(xml)
+
+    if result:
+        return result
+    else:
+        return sch.get_validation_errors(xml)
 
 
-def not_validated(collection, journal_issn=None):
+def not_validated(collection, journal_issn=None, skip=0, limit=10):
     """
     Implements an iterable article PID list not validated on SciELO.
     validated_scielo = False
@@ -41,10 +53,10 @@ def not_validated(collection, journal_issn=None):
     if journal_issn:
         fltr.update({'journal': journal_issn})
 
-    return find(fltr, collection).limit(10)
+    return find(fltr, collection, skip=skip, limit=limit)
 
 
-def validated(collection, journal_issn=None):
+def validated(collection, journal_issn=None, skip=0, limit=10):
     """
     Implements an iterable article PID list eligible to be send to WoS.
     validated_scielo = True
@@ -57,10 +69,10 @@ def validated(collection, journal_issn=None):
     if journal_issn:
         fltr.update({'journal': journal_issn})
 
-    return find(fltr, collection)
+    return find(fltr, collection, skip=skip, limit=limit)
 
 
-def sent_to_wos(collection, journal_issn=None):
+def sent_to_wos(collection, journal_issn=None, skip=0, limit=10):
     """
     Implements an iterable article PID list cotaining docs already sento to wos.
     sent_wos = True
@@ -71,4 +83,4 @@ def sent_to_wos(collection, journal_issn=None):
     if journal_issn:
         fltr.update({'journal': journal_issn})
 
-    return find(fltr, collection)
+    return find(fltr, collection, skip=skip, limit=limit)
