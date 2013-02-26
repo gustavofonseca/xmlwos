d20167ff18a8d0f502882b1c8838e0b66cc8998b
fabiobatalha <fabiobatalha@gmail.com>
2013-02-26 13:47:56 -0300

    Ajustes de processamento de sincronizaçào com lista de status do WoS, correção de bug de montagem de arquivo XML, correção de bug de definição de issns do periódico no scielodata

--- export2wos/run.py
+++ export2wos/run.py
@@ -33,22 +33,23 @@ for issn in issns:
     xml_file_name = "tmp/xml/SciELO_{0}_{1}.xml".format(now, issn)
 
     xml = ''
+    xmlr = ''
     if not os.path.exists(xml_file_name):
         index_document = 0
         for document in documents:
             index_document = index_document + 1
-            xml = tools.validate_xml(coll, document, issn)
+            xml = tools.validate_xml(coll, document['code'], issn, api_host='192.168.1.76')
             if xml:
                 parser = etree.XMLParser(remove_blank_text=True)
                 root = etree.fromstring(xml, parser)
-                xml = xml + etree.tostring(root.getchildren()[0])
+                xmlr = xmlr + etree.tostring(root.getchildren()[0])
 
         if xml:
-            xml_file = open(xml_file_name, 'a')
+            xml_file = open(xml_file_name, 'w')
             xml_file.write('<articles xmlns:xlink="http://www.w3.org/1999/xlink" '\
                         'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" '\
                         'dtd-version="1.06">')
-            xml_file.write(xml)
+            xml_file.write(xmlr)
             xml_file.write("</articles>")
             xml_file.close()
 
